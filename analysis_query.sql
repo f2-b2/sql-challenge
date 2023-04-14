bf659f2cdae8c1c40007848e1dfd5b20813b0ed2
Fiona Blackmon-Burns <blackmonburnsfiona@gmail.com>
2023-04-13 23:22:06 -0400

    SQL upload

---
+++ analysis_query.sql
@@ -0,0 +1,69 @@
+﻿--1. List the employee number, last name, first name, sex, and salary of each employee.
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees AS e
+	JOIN salaries AS s 
+		ON e.emp_no = s.emp_no;
+		
+
+--2. List the first name, last name, and hire date for the employees who were hired in 1986.
+SELECT e.first_name, e.last_name, e.hire_date
+FROM employees AS e
+WHERE DATE_PART('Year', e.hire_date) = '1986'
+
+
+--3. List the manager of each department along with their 
+--department number, department name, employee number, last name, and first name.
+SELECT dm.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+FROM employees AS e
+	JOIN dept_manager AS dm
+		ON e.emp_no = dm.emp_no
+	JOIN departments AS d
+		ON dm.dept_no = d.dept_no
+
+
+--4. List the department number for each employee along with that employee’s 
+--employee number, last name, first name, and department name.
+SELECT  e.emp_no, e.last_name, e.first_name, de.dept_no, d.dept_name
+FROM employees AS e
+	LEFT JOIN dept_emp AS de 
+		ON e.emp_no = de.emp_no
+	LEFT JOIN departments AS d 
+		ON de.dept_no = d.dept_no
+
+
+--5. List first name, last name, and sex of each employee whose 
+-- first name is Hercules and whose last name begins with the letter B.
+SELECT e.first_name, e.last_name, e.sex
+FROM employees AS e
+WHERE first_name = 'Hercules'
+AND last_name Like 'B%';
+
+
+--6. List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+	LEFT JOIN dept_emp AS de 
+		ON e.emp_no = de.emp_no
+	LEFT JOIN departments AS d
+		ON de.dept_no = d.dept_no
+	WHERE d.dept_name = 'Sales'
+
+
+--7. List each employee in the Sales and Development departments, 
+--including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees AS e
+	LEFT JOIN dept_emp AS de 
+		ON e.emp_no = de.emp_no
+	LEFT JOIN departments AS d
+		ON de.dept_no = d.dept_no
+	WHERE d.dept_name = 'Sales'
+	OR d.dept_name = 'Development'
+
+
+--8. List the frequency counts, in descending order, of all the employee last names 
+--(that is, how many employees share each last name).
+SELECT last_name, count(last_name) AS employee_count
+FROM employees
+GROUP BY last_name
+ORDER BY employee_count DESC
---
+++ table_schema.sql
@@ -0,0 +1,63 @@
+﻿-- Drop tables if they exist
+DROP TABLE IF EXISTS Titles CASCADE;
+DROP TABLE IF EXISTS Employees CASCADE;
+DROP TABLE IF EXISTS Departments CASCADE;
+DROP TABLE IF EXISTS Salaries;
+DROP TABLE IF EXISTS Dept_Manager;
+DROP TABLE IF EXISTS Dept_Emp;
+
+--Create tables
+CREATE TABLE Titles (
+    title_id varchar(255)   NOT NULL,
+    title varchar(255)   NOT NULL,
+    PRIMARY KEY (title_id)
+);
+
+CREATE TABLE Employees (
+    emp_no int   NOT NULL,
+    emp_title_id varchar(255)   NOT NULL,
+    birth_date date   NOT NULL,
+    first_name varchar(255)   NOT NULL,
+    last_name varchar(255)   NOT NULL,
+    sex varchar(255)   NOT NULL,
+    hire_date date   NOT NULL,
+    PRIMARY KEY (emp_no),
+    FOREIGN KEY(emp_title_id)
+        REFERENCES Titles (title_id)
+);
+
+CREATE TABLE Departments (
+    dept_no varchar(255)   NOT NULL,
+    dept_name varchar(255)   NOT NULL,
+    PRIMARY KEY (dept_no)
+);
+ 
+CREATE TABLE Salaries (
+    emp_no int   NOT NULL,
+    salary int   NOT NULL,
+    PRIMARY KEY (emp_no),
+    FOREIGN KEY(emp_no)
+        REFERENCES Employees (emp_no)
+);
+
+CREATE TABLE Dept_Manager (
+    dept_no varchar(255)   NOT NULL,
+    emp_no int   NOT NULL,
+	PRIMARY KEY(dept_no, emp_no),
+    FOREIGN KEY(dept_no)
+        REFERENCES Departments (dept_no),
+    FOREIGN KEY(emp_no)
+        REFERENCES Employees (emp_no)
+);
+
+CREATE TABLE Dept_Emp (
+    emp_no int   NOT NULL,
+    dept_no varchar(255)   NOT NULL,
+	PRIMARY KEY(emp_no, dept_no),
+    FOREIGN KEY(emp_no)
+        REFERENCES Employees (emp_no),
+    FOREIGN KEY(dept_no)
+        REFERENCES Departments (dept_no)
+);
+
+
